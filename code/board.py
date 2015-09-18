3a4557fa3857879b56fbb2cfcd56ffa705c32f68
johnsona9 <adamjohnson35@gmail.com>
2015-09-17 20:01:24 -0400

    removing pyc files. board can now add pieces to certain positions

--- code/board.py
+++ code/board.py
@@ -1,13 +1,20 @@
 class board(object):
     
     
+    
     def __init__(self):
-        self.something = "cool"
-        
+        self.squares = [[], [], [], [], [], [], [], []]
+        for rank in self.squares:
+            for x in range(0,8):
+                rank.append(None)
+                
+    def placePiece(self, position, piece):
+        self.squares[position[0]][position[1]] = piece
+        print self.squares
     # def getAttacked(self):
 #
 #     def setupGame(self):
         
     def isFull(self, position):
-        return True
+        return not self.squares[position[0]][position[1]]
         #if the given position has a piece return true
--- code/main.py
+++ code/main.py
@@ -2,10 +2,16 @@ from king import king
 from board import board
 from knight import knight
 from pawn import pawn
+board = board()
 
-king = king([1,1], "white", board())
-knight = knight([1,1], "black", board())
-pawn = pawn([2,2], "white", board())
+
+king = king([1,1], "white", board)
+knight = knight([1,1], "black", board)
+pawn = pawn([2,2], "white", board)
+
+board.placePiece([1,1], king)
+
 king.attackedSquares()
 knight.attackedSquares()
 pawn.attackedSquares()
+board.isFull([1,1])
