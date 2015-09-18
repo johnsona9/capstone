4cc04ca6a7e7b4f7b5b3ba5cda7f6669b8fd9be2
johnsona9 <adamjohnson35@gmail.com>
2015-09-18 18:11:13 -0400

    board now accounts for offset of array indexes

--- code/board.py
+++ code/board.py
@@ -9,12 +9,12 @@ class board(object):
                 rank.append(None)
                 
     def placePiece(self, position, piece):
-        self.squares[position[0]][position[1]] = piece
-        print self.squares
+        self.squares[position[0] - 1][position[1] - 1] = piece
+
     # def getAttacked(self):
 #
 #     def setupGame(self):
         
     def isFull(self, position):
-        return not self.squares[position[0]][position[1]]
+        return not self.squares[position[0] - 1][position[1] - 1] == None
         #if the given position has a piece return true
--- code/main.py
+++ code/main.py
@@ -10,8 +10,10 @@ knight = knight([1,1], "black", board)
 pawn = pawn([2,2], "white", board)
 
 board.placePiece([1,1], king)
+board.placePiece([2,2], pawn)
 
 king.attackedSquares()
 knight.attackedSquares()
 pawn.attackedSquares()
-board.isFull([1,1])
+print board.isFull([1,1])
+print board.isFull([2,2])
