0815c41981b68956caaa20670078a87ef85cf3b1
johnsona9 <adamjohnson35@gmail.com>
2015-09-19 13:30:31 -0400

    rooks attack properly

--- code/main.py
+++ code/main.py
@@ -3,16 +3,14 @@ from board import board
 from knight import knight
 from pawn import pawn
 from bishop import bishop
+from rook import rook
 
 board = board()
 king = king([1,1], "white", board)
 knight = knight([1,1], "black", board)
 pawn = pawn([2,2], "white", board)
 bishop = bishop([3,3], "white", board)
-#board.placePiece([1,1], king)
+rook = rook([2,6], "black", board)
 board.placePiece([2,2], pawn)
 board.placePiece([6,6], knight)
-print bishop.attackedSquares()
-# king.attackedSquares()
-# knight.attackedSquares()
-# pawn.attackedSquares()
+
--- code/rook.py
+++ code/rook.py
@@ -1,6 +1,72 @@
+from board import board
 class rook(object):
     
-    def __init__(self, position):
-        
+    file = 0
+    rank = 0
+    color = None
+    board = None
+    
+    
+    def __init__(self, position, color, board):
+        self.file = position[0]
+        self.rank = position[1]
+        self.color = color
+        self.board = board
     
     def attackedSquares(self):
+        attacking = []
+        finalAttacking = []
+        
+        #right side
+        x = 1
+        position = (self.file + x, self.rank)
+        while position[0] > 0 and position[0] < 9 and position[1] > 0 and position[1] < 9:
+            if self.board.isFull(position):
+                attacking.append(position)
+                break #from while
+            else:
+                attacking.append(position)
+            x += 1
+            position = (self.file + x, self.rank)
+            
+        #left side
+        x = 1
+        position = (self.file - x, self.rank)
+        while position[0] > 0 and position[0] < 9 and position[1] > 0 and position[1] < 9:
+            if self.board.isFull(position):
+                attacking.append(position)
+                break #from while
+            else:
+                attacking.append(position)
+            x += 1
+            position = (self.file - x, self.rank)
+            
+        #up 
+        x = 1
+        position = (self.file, self.rank + x)
+        while position[0] > 0 and position[0] < 9 and position[1] > 0 and position[1] < 9:
+            if self.board.isFull(position):
+                attacking.append(position)
+                break #from while
+            else:
+                attacking.append(position)
+            x += 1
+            position = (self.file, self.rank + x)
+            
+        #down 
+        x = 1
+        position = (self.file, self.rank - x)
+        while position[0] > 0 and position[0] < 9 and position[1] > 0 and position[1] < 9:
+            if self.board.isFull(position):
+                attacking.append(position)
+                break #from while
+            else:
+                attacking.append(position)
+            x += 1
+            position = (self.file, self.rank - x)
+            
+        for square in attacking:
+            if (square[0] > 0 and square[0] < 9 and square[1] > 0 and square[1] < 9):
+                finalAttacking.append(square)
+
+        return finalAttacking
