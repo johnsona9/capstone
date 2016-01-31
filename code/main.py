d865dade1843918b40c6d2a69ab919c7a055998d
johnsona <adamjohnson35@gmail.com>
2016-01-31 17:43:01 -0500

    User input parsing works
    Properly parsing user input and moving pieces based on it

--- code/main.py
+++ code/main.py
@@ -1,3 +1,4 @@
+from piece import piece
 from pawn import pawn
 from rook import rook
 from knight import knight
@@ -7,16 +8,24 @@ from king import king
 from board import board
 from game import game
 
-squareDictionary = {'a1': [1,1], 'a2': [1,2], 'a3': [1,3], 'a4': [1,4], 'a5': [1,5], 'a6': [1,6], 'a7': [1,7], 'a8': [1,8], 
-                    'b1': [2,1], 'b2': [2,2], 'b3': [2,3], 'b4': [2,4], 'b5': [2,5], 'b6': [2,6], 'b7': [2,7], 'b8': [2,8],
-                    'c1': [3,1], 'c2': [3,2], 'c3': [3,3], 'c4': [3,4], 'c5': [3,5], 'c6': [3,6], 'c7': [3,7], 'c8': [3,8],
-                    'd1': [4,1], 'd2': [4,2], 'd3': [4,3], 'd4': [4,4], 'd5': [4,5], 'd6': [4,6], 'd7': [4,7], 'd8': [4,8],
-                    'e1': [5,1], 'e2': [5,2], 'e3': [5,3], 'e4': [5,4], 'e5': [5,5], 'e6': [5,6], 'e7': [5,7], 'e8': [5,8],
-                    'f1': [6,1], 'f2': [6,2], 'f3': [6,3], 'f4': [6,4], 'f5': [6,5], 'f6': [6,6], 'f7': [6,7], 'f8': [6,8],
-                    'g1': [7,1], 'g2': [7,2], 'g3': [7,3], 'g4': [7,4], 'g5': [7,5], 'g6': [7,6], 'g7': [7,7], 'g8': [7,8],
-                    'h1': [8,1], 'h2': [8,2], 'h3': [8,3], 'h4': [8,4], 'h5': [8,5], 'h6': [8,6], 'h7': [8,7], 'h8': [8,8]}
+squareDictionary = {'a1': [1,1], 'a2': [2,1], 'a3': [3,1], 'a4': [4,1], 'a5': [5,1], 'a6': [6,1], 'a7': [7,1], 'a8': [8,1], 
+                    'b1': [1,2], 'b2': [2,2], 'b3': [3,2], 'b4': [4,2], 'b5': [5,2], 'b6': [6,2], 'b7': [7,2], 'b8': [8,2],
+                    'c1': [1,3], 'c2': [2,3], 'c3': [3,3], 'c4': [4,3], 'c5': [5,3], 'c6': [6,3], 'c7': [7,3], 'c8': [8,3],
+                    'd1': [1,4], 'd2': [2,4], 'd3': [3,4], 'd4': [4,4], 'd5': [5,4], 'd6': [6,4], 'd7': [7,4], 'd8': [8,4],
+                    'e1': [1,5], 'e2': [2,5], 'e3': [3,5], 'e4': [4,5], 'e5': [5,5], 'e6': [6,5], 'e7': [7,5], 'e8': [8,5],
+                    'f1': [1,6], 'f2': [2,6], 'f3': [3,6], 'f4': [4,6], 'f5': [5,6], 'f6': [6,6], 'f7': [7,6], 'f8': [8,6],
+                    'g1': [1,7], 'g2': [2,7], 'g3': [3,7], 'g4': [4,7], 'g5': [5,7], 'g6': [6,7], 'g7': [7,7], 'g8': [8,7],
+                    'h1': [1,8], 'h2': [2,8], 'h3': [3,8], 'h4': [4,8], 'h5': [5,8], 'h6': [6,8], 'h7': [7,8], 'h8': [8,8]}
 board = board()
 game = game(board)
+
+def requestFirst(prompt):
+    value = raw_input(prompt)
+    if isinstance(board.getPiece(squareDictionary[value]), piece):
+        return value
+    else:
+        print "There is no piece on that square, please pick another square."
+        return request(prompt)
 
 # board.getPiece([8,5]).movePiece([5,5])
 # for e in reversed(board.getAttacked()):
@@ -24,10 +33,8 @@ game = game(board)
 for e in reversed(board.printBoard()):
     print e
 
-
-fromSquare = raw_input("Piece moves from square: ")
-toSquare = raw_input("Piece moves to square: ")
-print board.getPiece(squareDictionary[fromSquare]) 
-print board.getPiece(squareDictionary[fromSquare]).color
-print board.getPiece(squareDictionary[toSquare]) 
-print board.getPiece(squareDictionary[toSquare]).color
+fromSquare = squareDictionary[requestFirst("Piece moving from square: ")]
+toSquare = squareDictionary[raw_input("Piece moves to square: ")]
+board.getPiece(fromSquare).movePiece(toSquare)
+for e in reversed(board.printBoard()):
+    print e
