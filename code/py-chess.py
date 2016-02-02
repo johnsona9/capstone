1beb9ac21d22de80806ca5d3d67fa2a6b1e5c7e9
johnsona <adamjohnson35@gmail.com>
2016-02-02 13:35:34 -0500

    Counting number of time D5 is attacked by white

--- code/py-chess.py
+++ code/py-chess.py
@@ -5,5 +5,8 @@ move = chess.Move(chess.SQUARE_NAMES.index("e2"), chess.SQUARE_NAMES.index("e4")
 if move in board.legal_moves:
 	board.push(chess.Move(chess.SQUARE_NAMES.index("e2"), chess.SQUARE_NAMES.index("e4")))
 print board
-print board.legal_moves
-print move
+print len(board.attackers(chess.WHITE, chess.D5))
+board.push(chess.Move(chess.SQUARE_NAMES.index("e7"), chess.SQUARE_NAMES.index("e5")))
+board.push(chess.Move(chess.SQUARE_NAMES.index("f1"), chess.SQUARE_NAMES.index("c4")))
+print board
+print len(board.attackers(chess.WHITE, chess.D5))
