79c6b369fbed299631ed7cc93e5eaacdffcc85ec
johnsona <adamjohnson35@gmail.com>
2016-02-04 11:07:12 -0500

    Python-chess ready for testing

--- src/pymain.py
+++ src/pymain.py
@@ -7,34 +7,42 @@ board = chess.Board()
 
 def lighter():
     # for every square we need to get the 
-    for square in chess.SQUARES:
+    for x in chess.SQUARES:
         isAttacked = False
-        whiteAttacks = len(board.attackers(chess.WHITE, square))
+        whiteAttacks = len(board.attackers(chess.WHITE, chess.SQUARES[x]))
         if whiteAttacks > 0:
             isAttacked = True
-        blackAttacks = len(board.attackers(chess.BLACK, square))
+        blackAttacks = len(board.attackers(chess.BLACK, chess.SQUARES[x]))
         value = whiteAttacks - blackAttacks
-        pos = square.rank_index() * 8 + square.file_index()
-        if value < 0:
-            bus.write_byte_data(address, 0, 64 + pos)
-        elif value == 0 && isAttacked:
-            bus.write_byte_data(address, 0, 192 + pos)
-        elif value > 0:
-            bus.write_byte_data(address, 0, 128 + pos)
-        else:
-            bus.write_byte_data(address, 0, pos)
+        #
+        # rank = x / 8
+        # file = x % 8
+        # pos = rank * 8 + file
+        # if value < 0:
+#             # bus.write_byte_data(address, 0, 64 + pos)
+#         elif value == 0 and isAttacked:
+#             # bus.write_byte_data(address, 0, 192 + pos)
+#         elif value > 0:
+#             #bus.write_byte_data(address, 0, 128 + pos)
+#         else:
+            #bus.write_byte_data(address, 0, pos)
 
 def main():
-    lighter()
-    fromSquare = raw_input("Piece moving from square: ")
-    toSquare = raw_input("Piece moving to square: ")
-    move = chess.Move(chess.SQUARE_NAMES.index(fromSquare), chess.SQUARE_NAMES.index(toSquare))
-    while move not in board.legal_moves:
-        print "You made an illegal move, please try again."
+    while 1:
+        lighter()
         fromSquare = raw_input("Piece moving from square: ")
         toSquare = raw_input("Piece moving to square: ")
         move = chess.Move(chess.SQUARE_NAMES.index(fromSquare), chess.SQUARE_NAMES.index(toSquare))
-    board.push(move)
+        while move not in board.legal_moves:
+            print "You made an illegal move, please try again."
+            fromSquare = raw_input("Piece moving from square: ")
+            toSquare = raw_input("Piece moving to square: ")
+            move = chess.Move(chess.SQUARE_NAMES.index(fromSquare), chess.SQUARE_NAMES.index(toSquare))
+        board.push(move)
+        print board
+        
+        
+main()
         
     
     
