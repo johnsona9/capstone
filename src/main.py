c682d8c18747f362e6bca4cf0fcf171d7651a9d6
johnsona <adamjohnson35@gmail.com>
2016-02-15 13:56:19 -0500

    Documentation for pylint

--- src/main.py
+++ src/main.py
@@ -1,50 +1,53 @@
+import time
+from subprocess import Popen
+import sys
+from multiprocessing import Process
+
+
 import chess
 import smbus
-import time
 from pystockfish import *
-from multiprocessing import Process
-from datetime import datetime
-from subprocess import Popen, PIPE
-import sys
 
-board = chess.Board()
-bus = smbus.SMBus(1)
-address = 0x04
-delay = 0.0005
+
+BOARD = chess.Board()
+BUS = smbus.SMBus(1)
+ADDRESS = 0x04
+DELAY = 0.0005
 
 lastMove = False
-moves = []
-engine = Engine(depth=10)
-stockfish = False
-if (len(sys.argv) > 1 and int(sys.argv[1]) == 1):
-    stockfish = True
+MOVES = []
+ENGINE = Engine(depth=10)
+STOCKFISH = False
+if len(sys.argv) > 1 and int(sys.argv[1]) == 1:
+    STOCKFISH = True
 
 
 def lighter():
-    if board.is_game_over():
-        if board.is_checkmate():
+    if BOARD.is_game_over():
+        if BOARD.is_checkmate():
             if lastMove:
-                singleColor("green")
+                single_color("green")
             else:
-                singleColor("red")
-        elif board.is_stalemate():
-            singleColor("blue")
+                single_color("red")
+        elif BOARD.is_stalemate():
+            single_color("blue")
 
     else:
         for x in chess.SQUARES:
             isAttacked = False
-            whiteAttacks = len(board.attackers(chess.WHITE, chess.SQUARES[x]))
+            whiteAttacks = len(BOARD.attackers(chess.WHITE, chess.SQUARES[x]))
             if whiteAttacks > 0:
                 isAttacked = True
-            blackAttacks = len(board.attackers(chess.BLACK, chess.SQUARES[x]))
+            blackAttacks = len(BOARD.attackers(chess.BLACK, chess.SQUARES[x]))
             value = whiteAttacks - blackAttacks
             rank = x / 8
-            file = x % 8
-            pos = rank * 8 + file
-            if (stockfish):
-                bus.write_byte(address, 3)
-                time.sleep(delay)
-                bus.write_byte(address, 0)
+            column = x % 8
+            pos = rank * 8 + column
+            if STOCKFISH:
+                time.sleep(DELAY)
+                BUS.write_byte(ADDRESS, 3)
+                time.sleep(DELAY)
+                BUS.write_byte(ADDRESS, 0)
             if value < 0:
                 data = [0, 64 + pos]
             elif value == 0 and isAttacked:
@@ -55,54 +58,55 @@ def lighter():
                 data = [0, pos]
             try:
                 for i in data:
-                    time.sleep(delay)
-                    bus.write_byte(address, i)
+                    time.sleep(DELAY)
+                    BUS.write_byte(ADDRESS, i)
             except IOError:
-    		    Popen("i2cdetect -y 1 >/dev/null", shell=True)
+                Popen("i2cdetect -y 1 >/dev/null", shell=True)
 
 
 def main():
     lighter()
-    while not board.is_game_over():
-        if (stockfish):
-            runStockfish()
-        move = requestMove()
-        board.push(move)
+    while not BOARD.is_game_over():
+        if STOCKFISH:
+            run_stockfish()
+        move = request_move()
+        BOARD.push(move)
         global lastMove
         lastMove = not lastMove
         lighter()
-        moves.append(move.uci())
-        print board
+        MOVES.append(move.uci())
+        print BOARD
     lighter()
 
 
-def runStockfish():
-    engine.setposition(moves)
-    p = Process(target=bestMove)
+def run_stockfish():
+    ENGINE.setposition(MOVES)
+    p = Process(target=best_move)
     p.start()
     p.join()
 
 
-def bestMove():
-    move = engine.bestmove()["move"]
+def best_move():
+    move = ENGINE.bestmove()["move"]
     fromSquare = move[:2]
     toSquare = move[2:]
-    bus.write_byte_data(address, 0, 1)
-    time.sleep(delay)
-    bus.write_byte_data(address, 0, chess.SQUARE_NAMES.index(fromSquare))
-    time.sleep(delay)
-    bus.write_byte_data(address, 0, 2)
-    time.sleep(delay)
-    bus.write_byte_data(address, 0, chess.SQUARE_NAMES.index(toSquare))
+    time.sleep(DELAY)
+    BUS.write_byte_data(ADDRESS, 0, 1)
+    time.sleep(DELAY)
+    BUS.write_byte_data(ADDRESS, 0, chess.SQUARE_NAMES.index(fromSquare))
+    time.sleep(DELAY)
+    BUS.write_byte_data(ADDRESS, 0, 2)
+    time.sleep(DELAY)
+    BUS.write_byte_data(ADDRESS, 0, chess.SQUARE_NAMES.index(toSquare))
 
 
-def checkPromotions(fromSquare, toSquare):
-    if (board.piece_at(chess.SQUARE_NAMES.index(fromSquare)) == chess.Piece(chess.PAWN, chess.WHITE) and toSquare[1] == '8') or (board.piece_at(chess.SQUARE_NAMES.index(fromSquare)) == chess.Piece(chess.PAWN, chess.BLACK) and toSquare[1] == '1'):
-        return promotionQuery()
+def check_promotions(fromSquare, toSquare):
+    if (BOARD.piece_at(chess.SQUARE_NAMES.index(fromSquare)) == chess.Piece(chess.PAWN, chess.WHITE) and toSquare[1] == '8') or (BOARD.piece_at(chess.SQUARE_NAMES.index(fromSquare)) == chess.Piece(chess.PAWN, chess.BLACK) and toSquare[1] == '1'):
+        return promotion_query()
     else:
         return 0
 
-def requestMove():
+def request_move():
     fromSquare = raw_input("Piece moving from square: ")
     while fromSquare not in chess.SQUARE_NAMES:
         print "You entered a value that is not a square, please enter another square."
@@ -111,12 +115,12 @@ def requestMove():
     while toSquare not in chess.SQUARE_NAMES:
         print "You entered a value that is not a square, please enter another square."
         toSquare = raw_input("Piece moving to square: ")
-    promotion = checkPromotions(fromSquare, toSquare)
+    promotion = check_promotions(fromSquare, toSquare)
     if promotion == 0:
         move = chess.Move(chess.SQUARE_NAMES.index(fromSquare), chess.SQUARE_NAMES.index(toSquare))
     else:
         move = chess.Move(chess.SQUARE_NAMES.index(fromSquare), chess.SQUARE_NAMES.index(toSquare), promotion)
-    while move not in board.legal_moves:
+    while move not in BOARD.legal_moves:
         print "You made an illegal move, please try again."
         fromSquare = raw_input("Piece moving from square: ")
         while fromSquare not in chess.SQUARE_NAMES:
@@ -126,7 +130,7 @@ def requestMove():
         while toSquare not in chess.SQUARE_NAMES:
             print "You entered a value that is not a square, please enter another square."
             toSquare = raw_input("Piece moving to square: ")
-        promotion = checkPromotions(fromSquare, toSquare)
+        promotion = check_promotions(fromSquare, toSquare)
         if promotion == 0:
             move = chess.Move(chess.SQUARE_NAMES.index(fromSquare), chess.SQUARE_NAMES.index(toSquare))
         else:
@@ -134,19 +138,19 @@ def requestMove():
     return move
 
 
-def singleColor(color):
+def single_color(color):
     colors = {"red" : 64, "green": 128, "blue": 192}
     for x in chess.SQUARES:
         data = [0, x + colors[color]]
-    	try:
+        try:
             for i in data:
-                time.sleep(delay)
-                bus.write_byte(address, i)
+                time.sleep(DELAY)
+                BUS.write_byte(ADDRESS, i)
         except IOError:
-    	    Popen("i2cdetect -y 1 >/dev/null", shell=True)
+            Popen("i2cdetect -y 1 >/dev/null", shell=True)
 
 
-def promotionQuery():
+def promotion_query():
     options = {'q': 5, 'r': 4, 'b': 3, 'n': 2}
     print "Promotion options are:"
     print "q : queen"
