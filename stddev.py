1f8a2cdda74b96e489aeb282433f5c0223af18bf
johnsona <adamjohnson35@gmail.com>
2016-03-03 11:49:40 -0500

    Removing divide by 200 from stddev file
    No idea why I was dividing by 200 in the first place.

--- stddev.py
+++ stddev.py
@@ -7,6 +7,6 @@ a = line.split(',')
 for x in range(0, len(a)):
     a[x] = float(a[x])
 print len(a)
-print statistics.mean(a) * 5 / 1000
-print statistics.stdev(a) * 5 / 1000
+print statistics.mean(a)
+print statistics.stdev(a)
 f.close()
